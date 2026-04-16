2415c6664fbd9727c9cc719d5e42d897c4aa9aba
Emmanuel Ibarra <emmanuelibarraruano@gmail.com>
2026-04-16 07:58:50 -0600

    ultima version

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
 from flask_migrate import Migrate
+from models import db
 from maestros import maestros_bp
-from models import db, Alumnos
+from alumnos import alumnos_bp
+from cursos import cursos_bp
 import forms
 
 app = Flask(__name__)
@@ -16,108 +18,13 @@ migrate = Migrate(app, db)
 
 # Register Blueprints
 app.register_blueprint(maestros_bp)
+app.register_blueprint(alumnos_bp)
+app.register_blueprint(cursos_bp)
 
 @app.route("/", methods=["GET"])
 @app.route("/index")
 def index():
     return render_template("bienvenida.html")
-
-@app.route("/alumnos/lista", methods=["GET"])
-def lista_alumnos():
-    alumno = Alumnos.query.all()
-    return render_template("alumnos/index.html", alumno=alumno)
-
-@app.route("/alumnos", methods=['GET', 'POST'])
-def alumnos():
-    create_form = forms.UserForm(request.form)
-    if request.method == 'POST' and create_form.validate():
-        alum = Alumnos(
-            nombre=create_form.nombre.data,
-            apaterno=create_form.apaterno.data,
-            amaterno=create_form.amaterno.data,
-            edad=create_form.edad.data,
-            email=create_form.email.data
-        )
-        db.session.add(alum)
-        db.session.commit()
-        flash("Alumno registrado exitosamente")
-        return redirect(url_for('lista_alumnos'))
-    return render_template("alumnos/crear.html", form=create_form)
-
-@app.route("/alumnos/modificar", methods=['GET', 'POST'])
-def modificar():
-    create_form = forms.UserForm(request.form)
-    id = request.args.get('id') or request.form.get('id')
-    alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
-
-    if request.method == 'GET':
-        if not alum1:
-            flash("Alumno no encontrado")
-            return redirect(url_for('lista_alumnos'))
-        create_form.id.data = alum1.id
-        create_form.nombre.data = alum1.nombre
-        create_form.apaterno.data = alum1.apaterno
-        create_form.amaterno.data = alum1.amaterno
-        create_form.edad.data = alum1.edad
-        create_form.email.data = alum1.email
-
-    if request.method == 'POST' and create_form.validate():
-        if alum1:
-            alum1.nombre = create_form.nombre.data
-            alum1.apaterno = create_form.apaterno.data
-            alum1.amaterno = create_form.amaterno.data
-            alum1.edad = create_form.edad.data
-            alum1.email = create_form.email.data
-            db.session.commit()
-            flash("Alumno modificado correctamente")
-        return redirect(url_for('index'))
-        
-    return render_template("alumnos/modificar.html", form=create_form)
-
-@app.route('/alumnos/eliminar', methods=['GET', 'POST'])
-def eliminar():
-    create_form = forms.UserForm(request.form)
-    id = request.args.get('id') or request.form.get('id')
-    alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
-
-    if request.method == 'GET':
-        if not alum1:
-            flash("Alumno no encontrado")
-            return redirect(url_for('index'))
-        create_form.id.data = alum1.id
-        create_form.nombre.data = alum1.nombre
-        create_form.apaterno.data = alum1.apaterno
-        create_form.amaterno.data = alum1.amaterno
-        create_form.edad.data = alum1.edad
-        create_form.email.data = alum1.email
-
-    if request.method == 'POST':
-        if alum1:
-            db.session.delete(alum1)
-            db.session.commit()
-            flash("Alumno eliminado correctamente")
-        return redirect(url_for('index'))
-        
-    return render_template('alumnos/eliminar.html', form=create_form)
-
-@app.route("/alumnos/detalles", methods=['GET'])
-def detalles():
-    id = request.args.get('id')
-    alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
-    
-    if not alum1:
-        flash("Alumno no encontrado")
-        return redirect(url_for('lista_alumnos'))
-        
-    return render_template(
-        'alumnos/detalles.html',
-        id=alum1.id,
-        nombre=alum1.nombre,
-        apaterno=alum1.apaterno,
-        amaterno=alum1.amaterno,
-        edad=alum1.edad,
-        email=alum1.email
-    )
 
 @app.route("/usuarios", methods=["GET", "POST"])
 def usuario():
--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
-from wtforms import Form, StringField, IntegerField, EmailField
+from wtforms import Form, StringField, IntegerField, EmailField, TextAreaField, SelectField, HiddenField
 from wtforms import validators
 
 
 class UserForm(Form):
-    id = IntegerField("ID")
+    id = HiddenField("ID")
     nombre = StringField('Nombre', [
         validators.DataRequired(message="El campo nombre es requerido")
     ])
@@ -41,3 +41,24 @@ class MaestrosForm(Form):
         validators.DataRequired(message="El correo es requerido"),
         validators.Email(message="Ingrese un correo válido")
     ])
+
+
+class CursoForm(Form):
+    id = HiddenField('ID')
+    nombre = StringField('Nombre del Curso', [
+        validators.DataRequired(message="El nombre del curso es requerido"),
+        validators.Length(min=3, max=150)
+    ])
+    descripcion = TextAreaField('Descripción', [
+        validators.DataRequired(message="La descripción es requerida")
+    ])
+    maestro_id = SelectField('Maestro', coerce=int, validators=[
+        validators.DataRequired(message="Debe seleccionar un maestro")
+    ])
+
+
+class InscripcionForm(Form):
+    alumno_id = SelectField('Alumno', coerce=int, validators=[
+        validators.DataRequired(message="Debe seleccionar un alumno")
+    ])
+    curso_id = HiddenField('Curso')
